fc3aeb9981161e7b5b70eb970ac6fec4171c03bf
Frogvall <frjo02@gmail.com>
2026-07-17 21:00:37 +0200

    Facade elem totems

--- foundry/scripts/elemental-totems.js
+++ foundry/scripts/elemental-totems.js
@@ -1,79 +1,3 @@
-import { MODULE_ID } from "./core/constants.js";
-import { getContentKey } from "./core/documents.js";
-import { isPrimaryActiveGM } from "./core/users.js";
-
-import { parseHexColor } from "./core/colors.js";
-
-import {
-  buildTotemOptions,
-  getElementalTotemDefinitions,
-  getTotemName,
-  loadElementalTotemDefinitions,
-} from "./elemental-totems/definitions.js";
-import {
-  buildElementalTotemPlan,
-  validateElementalTotemPlanShape,
-} from "./elemental-totems/planning.js";
-
-import {
-  getPrimaryActiveGMUser,
-} from "./core/users.js";
-import {
-  calculateElementalTotemDistance,
-  collectElementalTotemPositions,
-  createElementalTotemPreviewDocument,
-  getElementalTotemPlacementRange,
-} from "./elemental-totems/placement.js";
-
-import {
-  ELEMENTAL_TOTEM_CONTENT_KEY,
-} from "./elemental-totems/constants.js";
-import {
-  getElementalTotemMessageContext,
-  isElementalTotemSpellTest,
-} from "./elemental-totems/messages.js";
-import {
-  configureCreatedElementalTotem,
-  deletePreviousElementalTotems,
-  executeElementalTotemCreation,
-  validateElementalTotemCreationRequest,
-} from "./elemental-totems/creation.js";
-
-import {
-  registerElementalTotemSocket,
-  requestElementalTotemCreation,
-} from "./elemental-totems/socket.js";
-
-export {
-  onCreateElementalTotemChatMessage,
-  onUpdateElementalTotemChatMessage,
-  shouldStartElementalTotemDialog,
-} from "./elemental-totems/dialogs.js";
-
-export {
-  registerElementalTotemSocket,
-};
-
-export {
-  configureCreatedElementalTotem,
-  deletePreviousElementalTotems,
-  executeElementalTotemCreation,
-  validateElementalTotemCreationRequest,
-};
-
-export { getPrimaryActiveGMUser };
-
-export {
-  getElementalTotemPlacementRange,
-};
-
-export {
-  buildTotemOptions,
-  loadElementalTotemDefinitions,
-  buildElementalTotemPlan,
-  validateElementalTotemPlanShape,
-};
-
 export {
   drawAllElementalTotemAuras,
   drawElementalTotemAura,
@@ -82,20 +6,37 @@ export {
   onUpdateElementalTotemAura,
 } from "./elemental-totems/aura.js";
 
-function redrawElementalTotemAuraForDocument(tokenDocument) {
-  if (canvas.scene?.id !== tokenDocument?.parent?.id) return;
+export {
+  buildTotemOptions,
+  loadElementalTotemDefinitions,
+} from "./elemental-totems/definitions.js";
 
-  const token = tokenDocument.object;
-  if (!token) return;
+export {
+  buildElementalTotemPlan,
+  validateElementalTotemPlanShape,
+} from "./elemental-totems/planning.js";
 
-  /*
-   * Let Foundry apply its own token refresh flags before measuring the
-   * token's current pixel width and height.
-   */
-  requestAnimationFrame(() => {
-    if (!token.destroyed) {
-      drawElementalTotemAura(token);
-    }
-  });
-}
+export {
+  getElementalTotemPlacementRange,
+} from "./elemental-totems/placement.js";
 
+export {
+  getPrimaryActiveGMUser,
+} from "./core/users.js";
+
+export {
+  configureCreatedElementalTotem,
+  deletePreviousElementalTotems,
+  executeElementalTotemCreation,
+  validateElementalTotemCreationRequest,
+} from "./elemental-totems/creation.js";
+
+export {
+  registerElementalTotemSocket,
+} from "./elemental-totems/socket.js";
+
+export {
+  onCreateElementalTotemChatMessage,
+  onUpdateElementalTotemChatMessage,
+  shouldStartElementalTotemDialog,
+} from "./elemental-totems/dialogs.js";
